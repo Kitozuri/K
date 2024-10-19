6205296d2b8e00ecc802f776725453207bd49b11
Kitozuri <nnguyenhoanggiahuy.71@gmail.com>
2024-10-19 18:17:18 +0700

    Initial Commit

---
+++ K/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  K
+//
+//  Created by Nguyễn Hoàng Gia Huy on 19/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
